ed55ff334826cf2c6aa96c1fa20da6ee5da5f3d1
ihsanvp <ihsanvp2738@gmail.com>
2024-07-23 21:00:38 +0530

    basic landing structure

---
+++ src/lib/components/SiteImage.svelte
@@ -0,0 +1,136 @@
+<svg
+	id="uuid-f8df215d-2683-445c-9843-65e73f6b3a0d"
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 241.7 186.07"
+	><defs
+		><style>
+			.uuid-f13be8c3-7350-4cad-b985-fd6d52c5ffa3 {
+				fill: #fff;
+			}
+			.uuid-6d0d5f7e-9bbb-4b74-9fb3-4bbdc77bc65b {
+				fill: #6d4f3c;
+			}
+			.uuid-7d7b9bf3-202f-4fc8-92c3-65cd41bfa507 {
+				stroke: #eee2da;
+			}
+			.uuid-7d7b9bf3-202f-4fc8-92c3-65cd41bfa507,
+			.uuid-d0140df8-6103-4122-ad87-c913e4523b6e {
+				fill: none;
+				stroke-miterlimit: 10;
+			}
+			.uuid-d0140df8-6103-4122-ad87-c913e4523b6e {
+				stroke: #6d4a3c;
+			}
+			.uuid-9307a730-6cd2-4cbe-b4da-87607532a78d {
+				fill: #eee3da;
+				stroke-width: 0px;
+			}
+		</style></defs
+	><g id="uuid-9785f4fc-f419-46ae-ba49-e3d08e620ddc"
+		><rect
+			class="uuid-f13be8c3-7350-4cad-b985-fd6d52c5ffa3"
+			x=".5"
+			y=".96"
+			width="240.7"
+			height="184.61"
+			rx="7.21"
+			ry="7.21"
+		></rect><rect
+			class="uuid-d0140df8-6103-4122-ad87-c913e4523b6e"
+			x=".5"
+			y=".96"
+			width="240.7"
+			height="184.61"
+			rx="7.21"
+			ry="7.21"
+		></rect><path
+			class="uuid-6d0d5f7e-9bbb-4b74-9fb3-4bbdc77bc65b"
+			d="M7.86.5h225.97c4.06,0,7.36,3.3,7.36,7.36v7.79H.5v-7.79C.5,3.8,3.8.5,7.86.5Z"
+		></path><path
+			class="uuid-d0140df8-6103-4122-ad87-c913e4523b6e"
+			d="M7.86.5h225.97c4.06,0,7.36,3.3,7.36,7.36v7.79H.5v-7.79C.5,3.8,3.8.5,7.86.5Z"
+		></path><circle class="uuid-9307a730-6cd2-4cbe-b4da-87607532a78d" cx="9.42" cy="8.52" r="3.39"
+		></circle><circle
+			class="uuid-9307a730-6cd2-4cbe-b4da-87607532a78d"
+			cx="19.58"
+			cy="8.52"
+			r="3.39"
+		></circle><circle
+			class="uuid-9307a730-6cd2-4cbe-b4da-87607532a78d"
+			cx="29.74"
+			cy="8.52"
+			r="3.39"
+		></circle><rect
+			class="uuid-9307a730-6cd2-4cbe-b4da-87607532a78d"
+			x="182.36"
+			y="78.33"
+			width="42.72"
+			height="4.64"
+		></rect><rect
+			class="uuid-9307a730-6cd2-4cbe-b4da-87607532a78d"
+			x="182.36"
+			y="86.44"
+			width="38.48"
+			height="4.64"
+		></rect><rect
+			class="uuid-9307a730-6cd2-4cbe-b4da-87607532a78d"
+			x="182.36"
+			y="95.04"
+			width="42.72"
+			height="4.64"
+		></rect><circle class="uuid-9307a730-6cd2-4cbe-b4da-87607532a78d" cx="202.65" cy="49" r="21.79"
+		></circle><rect
+			class="uuid-7d7b9bf3-202f-4fc8-92c3-65cd41bfa507"
+			x="16.12"
+			y="31.09"
+			width="148.74"
+			height="139.29"
+			rx="9.99"
+			ry="9.99"
+		></rect><rect
+			class="uuid-9307a730-6cd2-4cbe-b4da-87607532a78d"
+			x="51.05"
+			y="54.51"
+			width="74.32"
+			height="12.13"
+		></rect><rect
+			class="uuid-9307a730-6cd2-4cbe-b4da-87607532a78d"
+			x="58.88"
+			y="71.6"
+			width="58.65"
+			height="12.13"
+		></rect><rect
+			class="uuid-9307a730-6cd2-4cbe-b4da-87607532a78d"
+			x="51.05"
+			y="88.69"
+			width="74.32"
+			height="12.13"
+		></rect><rect
+			class="uuid-9307a730-6cd2-4cbe-b4da-87607532a78d"
+			x="65.74"
+			y="105.79"
+			width="44.93"
+			height="12.13"
+		></rect><rect
+			class="uuid-7d7b9bf3-202f-4fc8-92c3-65cd41bfa507"
+			x="54.78"
+			y="136.94"
+			width="65.85"
+			height="16.92"
+			rx="8.46"
+			ry="8.46"
+		></rect><line
+			class="uuid-7d7b9bf3-202f-4fc8-92c3-65cd41bfa507"
+			x1="146.82"
+			y1="40.23"
+			x2="154.3"
+			y2="47.7"
+		></line><line
+			class="uuid-7d7b9bf3-202f-4fc8-92c3-65cd41bfa507"
+			x1="154.3"
+			y1="40.23"
+			x2="146.82"
+			y2="47.7"
+		></line></g
+	></svg
+>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import SiteBackground from '$lib/components/SiteBackground.svelte';
+	import SiteImage from '$lib/components/SiteImage.svelte';
+</script>
+
+<section class="flex min-h-screen flex-col">
+	<nav class="h-32 w-full">
+		<div class="container mx-auto flex h-full items-center justify-between">
+			<div class="font-krona text-2xl font-bold uppercase tracking-[0.2em] text-orange-800">
+				Quarkus
+			</div>
+			<div class="flex gap-5">
+				<a class="rounded-md px-3 py-1 text-orange-800" href="/" data-mouse-stick>Services</a>
+				<a class="rounded-md px-3 py-1 text-orange-800" href="/" data-mouse-stick>About</a>
+				<a class="rounded-md px-3 py-1 text-orange-800" href="/" data-mouse-stick>Contact</a>
+			</div>
+		</div>
+	</nav>
+	<div class="container mx-auto flex flex-1 items-center py-12">
+		<div class="mb-40 grid grid-cols-2 items-center gap-10">
+			<div>
+				<div class="font-ibm mb-5 text-6xl text-orange-800" data-mouse-grow={80}>
+					Let's build a website <br /> that makes you the easy choice
+				</div>
+				<div class="font-fira mb-10 text-xl text-orange-800" data-mouse-grow={50}>
+					A generic website can make your unique business look like <br /> just another face in the crowd.
+				</div>
+				<button class="font-fira rounded-md bg-orange-900 px-10 py-3 text-white" data-mouse-stick
+					>Get Website</button
+				>
+			</div>
+			<div class="relative w-full max-w-2xl">
+				<SiteBackground />
+				<div class="absolute left-1/2 top-1/2 z-10 w-[40%] -translate-x-1/2 -translate-y-1/2">
+					<SiteImage />
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
